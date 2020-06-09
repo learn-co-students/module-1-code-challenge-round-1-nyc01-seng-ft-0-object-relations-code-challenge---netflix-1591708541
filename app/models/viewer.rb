7d17c686138ab255342fe389317b33193924265b
Leamsi Escribano <leamsi.esc@gmail.com>
2020-06-09 10:06:40 -0400

    Working on viewer class missing rate_movie method, testing.

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,33 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
+  end
+
+  def reviews
+    # returns an array of `Review` instances associated with the `Viewer` instance.
+    Review.all.select{|reivew| review.viewer == self}
+  end
+
+  def reviewed_movies
+    # returns an array of `Movie` instances reviewed by the `Viewer` instance.
+    reviews.map{|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    # returns `true` if the `Viewer` has reviewed this `Movie` (if there is a `Review` instance that has this `Viewer` and `Movie`), returns `false` otherwise
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    # a `Movie` instance and a rating (number) are passed in as arguments
+    # if the `Viewer` instance and the passed `Movie` instance are _not_ already associated, this method should create a new `Review` instance
+
+    # if this `Viewer` has already reviewed this `Movie`, assigns the new rating to the existing `Review` instance
   end
 
   def self.all
     @@all
   end
-  
+
 end
