8daefc7402b3f2b9e7f661b45815782314cd520b
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-09 09:47:12 -0400

    made initial changes to code

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,11 @@ def reload
   load 'config/environment.rb'
 end
 
+moneyball = Movie.new("moneyball")
+
+brandon = Viewer.new("brandon")
+
+good = Review.new("brandon","moneyball", "good")
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
