6b67248c1ae6486dd6cb9826b14a676fa3fe458f
Nazmul1216 <nazmul.kabir1216@gmail.com>
2020-06-09 10:44:58 -0400

    movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,11 @@
 class Movie
-  attr_accessor :title
+  attr_accessor :title, :review
 
   @@all = []
 
-  def initialize(title)
+  def initialize(title, review)
     @title = title
+    @review = review
     self.class.all << self
   end
 
@@ -15,4 +16,15 @@ class Movie
 def self.title
   @title
 end 
+def self.average_rating
+  self.rating / rating 
+end
+
+def highest_rated
+  max=-1
+  best_film=Review.rating
+  if max<best_film
+    max=best_film
+  end
+  max==best_film
 end
