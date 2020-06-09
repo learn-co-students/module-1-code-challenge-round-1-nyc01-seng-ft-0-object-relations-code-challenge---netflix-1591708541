1777165b8f26f35278237903bb12ae981ad666eb
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-06-09 09:50:04 -0400

    setup

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,37 @@
+# A `Movie` has many `Review`s. A `Viewer` has many `Review`s. A `Review` belongs to a `Viewer` and belongs to a `Movie`.
+# `Viewer` - `Movie` is a many to many relationship.
+
+# Deliverables use the notation `#` for instance methods, and `.` for class methods.
+
+#------------------------------------------------------------------------
+
+
+# - `Movie#initialize(title)`
+#   - `Movie` is initialized with a title (string)
+#   - title **can be** changed after the `Movie` is initialized
+
+# - `Movie#title`
+#   - returns the `Movie`'s title
+
+# - `Movie.all`
+#   - returns an array of all the `Movie` instances that have been initialized
+
+# - `Movie#reviews`
+#   - returns an array of all the `Review` instances for the `Movie`.
+
+# - `Movie#reviewers`
+#   - returns an array of all of the `Viewer` instances that reviewed the `Movie`.
+
+# - `Movie#average_rating`
+#   - returns the average of all ratings for the `Movie` instance
+#   - to average ratings, add all ratings together and divide by the total number of ratings.
+
+# - `Movie.highest_rated`
+#   - returns the `Movie` instance with the highest average rating.
+
+#------------------------------------------------------------------------
+
+
 class Movie
   attr_accessor :title
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,28 @@
+# A `Movie` has many `Review`s. A `Viewer` has many `Review`s. A `Review` belongs to a `Viewer` and belongs to a `Movie`.
+# `Viewer` - `Movie` is a many to many relationship.
+
+# Deliverables use the notation `#` for instance methods, and `.` for class methods.
+
+#------------------------------------------------------------------------
+
+
+# - `Review#initialize(viewer, movie, rating)`
+#   - `Review` is initialized with a `Viewer` instance, a `Movie` instance, and a rating (number)
+
+# - `Review#rating`
+#   - returns the rating for the `Review` instance
+
+# - `Review.all`
+#   - returns an array of all initialized `Review` instances
+
+# - `Review#viewer`
+#   - returns the `Viewer` instance associated with the `Review` instance
+
+# - `Review#movie`
+#   - returns the `Movie` instance associated with the `Review` instance
+#------------------------------------------------------------------------
+
+
 class Review
 
 
