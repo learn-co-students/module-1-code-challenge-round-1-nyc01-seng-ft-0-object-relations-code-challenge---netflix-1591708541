86e8b871e517e0e0d42a40cb32b7ebbcbea466a5
maxcroy1 <max.croy1@gmail.com>
2020-06-09 10:00:59 -0400

    Successfully tested initialization of new Movie classes

--- tools/console.rb
+++ tools/console.rb
@@ -10,9 +10,9 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-
-
-
+movie_one = Movie.new("Okja")
+movie_two = Movie.new("Snowpiercer")
+movie_three = Movie.new("Parasite")
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
 binding.pry
