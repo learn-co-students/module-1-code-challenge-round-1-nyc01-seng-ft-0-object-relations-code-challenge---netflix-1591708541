21db6cccccb308a75d761866ac465efdc9421df4
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-09 10:10:25 -0400

    what i have so far with an hour left

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
+    #sst class
+
+    atrr_reader :viewer, :movie, :rating
+
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @review = review
+        @movie = movie
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def viewer
+        #need to return viewer
+        #associated with review
+
+
+    end
+
+    def movie
+        #need to return movie 
+        #associated with review
+
+    end
 
 
 end
