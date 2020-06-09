b7d2b97d0ffdd652a15d2e3808745d9c42753ca9
maxcroy1 <max.croy1@gmail.com>
2020-06-09 09:59:16 -0400

    Created Aggregate and Association Methods for Movie class

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,8 +16,16 @@ class Movie
     self.reviews.map{|review| review.viewer}
   end
 
+  def average_rating
+    self.reviews.sum{|review| review.rating} / self.reviews.count
+  end
+
   def self.all
     @@all
+  end
+
+  def self.highest_rated
+    @@all.max{|movie_a, movie_b| movie_a.average_rating <=> movie_b.average_rating}
   end
 
 end
