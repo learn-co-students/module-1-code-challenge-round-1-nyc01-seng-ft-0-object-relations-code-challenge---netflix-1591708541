9ce642265868f0731581767144ce9a208a8aeff9
alexpac27 <alexpac27@gmail.com>
2020-06-09 09:53:20 -0400

    save

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,11 +5,28 @@ class Movie
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
+  def title
+    self.title
+  end
+
 end
+
+
+
+
+# #### Movie
+
+        # - `Movie#initialize(title)`
+        #   - `Movie` is initialized with a title (string)
+        #   - title **can be** changed after the `Movie` is initialized
+# - `Movie#title`
+#   - returns the `Movie`'s title
+# - `Movie.all`
+#   - returns an array of all the `Movie` instances that have been initialized
