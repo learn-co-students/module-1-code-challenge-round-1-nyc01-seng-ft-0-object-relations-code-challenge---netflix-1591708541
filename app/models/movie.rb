28493f3cec68f85003990963426fc18f70dbc4b7
Leamsi Escribano <leamsi.esc@gmail.com>
2020-06-09 09:58:07 -0400

    Finished Movie class, need to start testing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,8 +4,30 @@ class Movie
   @@all = []
 
   def initialize(title)
+    # A movie has many reviews and many viewers through reviews
     @title = title
-    self.class.all << self
+    @@all << self
+  end
+
+  def reviews
+    # returns an array of all the `Review` instances for the `Movie`.
+    Review.all.select{|review| review.movie == self}
+  end
+
+  def reviewers
+    # returns an array of all of the `Viewer` instances that reviewed the `Movie`
+    reviews.map{|review| review.viewer}
+  end
+
+  def average_rating
+    # returns the average of all ratings for the `Movie` instance
+    total_ratings = reviews.sum{|review| review.rating}
+    (total_ratings / reviews.size).to_f
+  end
+
+  def self.highest_rated
+    # returns the `Movie` instance with the highest average rating
+    @@all.max_by{|movie| movie.average_rating}
   end
 
   def self.all
