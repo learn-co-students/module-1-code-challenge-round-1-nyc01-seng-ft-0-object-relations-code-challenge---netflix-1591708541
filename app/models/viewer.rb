d4eca969def032694eeddb7cc031baba24178f96
dolly-d <dolly.desir@gmail.com>
2020-06-09 10:24:40 -0400

    first commit

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,33 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
   
+
+
+
+
 end
+
+
+
+# - `Viewer#initialize(username)`
+#   - `Viewer` is initialized with a username (string)
+#   - username **can be** changed after the Viewer is initialized
+# - `Viewer#username`
+#   - returns the Viewer's username
+# - `Viewer.all`
+#   - returns an array of all the Viewer instances that have been initialized
+
+# - `Viewer#reviewed_movie?(movie)`
+#   - a `Movie` instance is the only argument
+#   - returns `true` if the `Viewer` has reviewed this `Movie` (if there is a `Review` instance that has this `Viewer` and `Movie`), returns `false` otherwise
+# - `Viewer#rate_movie(movie, rating)`
+#   - a `Movie` instance and a rating (number) are passed in as arguments
+#   - if the `Viewer` instance and the passed `Movie` instance are _not_ already associated, this method should create a new `Review` instance
+#   - if this `Viewer` has already reviewed this `Movie`, assigns the new rating to the existing `Review` instance
--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,22 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+mia = Viewer.new("Mia")
+lisa = Viewer.new("Lisa")
+steve = Viewer.new("Steve")
+sam = Viewer.new("Sam")
+
+movie1 = Movie.new("Boyz In Da Hood")
+movie2 = Movie.new("Boomerang")
+movie3 = Movie.new("Blankman")
+movie4 = Movie.new("A Lowdown Dirty Shame")
+# movie5 = Movie.new("Bad Boys")
+# movie6 = Movie.new("The Last Dragon")
+
+review1 = Review.new(mia, movie1, 5)
+review2 = Review.new(lisa, movie2, 4)
+review3 = Review.new(steve, movie3, 3)
+review4 = Review.new(sam, movie4, 2)
 
 
 
