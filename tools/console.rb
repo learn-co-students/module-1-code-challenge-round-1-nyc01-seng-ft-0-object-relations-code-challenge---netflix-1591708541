2aac6da1593cd47d416a473b0a63e9df929f41db
dr-dolce14 <dolcemascop1@gmail.com>
2020-06-09 09:57:43 -0400

    initializers, readers and writers DONE

--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,14 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+movie1 = Movie.new("Jaws")
+movie2 = Movie.new("Pan's Labrynth")
+
+viewer1 = Viewer.new("Kate")
+viewer2 = Viewer.new("Paola")
+
+review1 = Review.new(viewer1, movie1, 8)
+review2 = Review.new(viewer2, movie2, 10)
 
 
 
