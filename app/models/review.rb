42b8a710c563ac635f1e26fabfdcb35bd397f028
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-06-09 09:44:01 -0400

    initializations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
 
+attr_accessor :viewer, :movie, :review
+
+@@all = []
+
+    def initialize(viewer, movie, review)
+        @viewer = viewer
+        @movie = movie
+        @review = review
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
