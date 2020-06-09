7ef7e0452e77165ef217a39d9dc6dd6d77d270c1
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-06-09 10:02:07 -0400

    worked on review deliverables

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,33 @@
 class Review
+  @@all = []
+  attr_reader :rating
+
+  def initialize(viewer, movie, rating)
+    @viewer = viewer
+    @movie = movie
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def viewer
+    Review.all.select do |i|
+      i.viewer == self
+    end
+  end
+
+  def movie
+    Review.all.select do |i|
+      i.movie == self
+    end
+  end
 
 
 end
+# - `Review#viewer`
+#   - returns the `Viewer` instance associated with the `Review` instance
+# - `Review#movie`
+#   - returns the `Movie` instance associated with the `Review` instance
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,9 @@ class Viewer
   def self.all
     @@all
   end
-  
+
 end
+# - `Viewer#reviews`
+#   - returns an array of `Review` instances associated with the `Viewer` instance.
+# - `Viewer#reviewed_movies`
+#   - returns an array of `Movie` instances reviewed by the `Viewer` instance.
