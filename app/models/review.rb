9dbb537fb04f733569da12dfaaa3f93777028931
orlandofat <orlandofat@gmail.com>
2020-06-09 09:53:56 -0400

    review initialize

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review
-
-
+attr_accessor :viewer, :movie, :rating
+@@all = []
+def initialize(viewer, movie, rating)
+    @viewer = viewer
+    @@all << self
+end
+def self.all
+    @@all
+end
 end
