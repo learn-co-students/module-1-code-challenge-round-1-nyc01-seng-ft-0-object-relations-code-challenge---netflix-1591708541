940552cbe4b9e38c27af1ea59bddf91ddc738493
RitaHartnell <mel.soriano6123@gmail.com>
2020-06-09 10:14:21 -0400

    added viewer aggregate & assoc. methods

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -20,6 +20,26 @@ class Viewer
     }
   end
 
+  def find_movie(movie)
+    self.reviewed_movies.find{ |m| m == movie }
+  end
+
+  def reviewed_movie?(movie)
+    if self.find_movie(movie)
+      true
+    else
+      false
+    end
+  end
+
+  def rate_movie(movie, rating)
+    if !self.reviewed_movie?(movie)
+      Review.new(self, movie, rating)
+    else
+      self.find_movie.rating = rating
+    end
+  end
+
   def self.all
     @@all
   end
